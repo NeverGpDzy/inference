a71167ab543ec0d8693786a2b76c9f15778615c6
NeverGpDzy <136807422+NeverGpDzy@users.noreply.github.com>
2024-10-21 21:38:40 +0800

    Update pytorch_SUT.py

--- language/bert/pytorch_SUT.py
+++ language/bert/pytorch_SUT.py
@@ -1,33 +1,4 @@
-# coding=utf-8
-# Copyright 2021 Arm Limited and affiliates.
-# Copyright (c) 2020 NVIDIA CORPORATION. All rights reserved.
-# Copyright 2018 The Google AI Language Team Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import array
-import json
-import os
-import sys
-sys.path.insert(0, os.path.join(os.getcwd(), "DeepLearningExamples", "PyTorch", "LanguageModeling", "BERT"))
-sys.path.insert(0, os.getcwd())
-
-import mlperf_loadgen as lg
-import numpy as np
-import torch
-import transformers
-from transformers import BertConfig, BertForQuestionAnswering
-from squad_QSL import get_squad_QSL
+import torch.quantization
 
 class BERT_PyTorch_SUT():
     def __init__(self, args):
@@ -59,6 +30,11 @@ class BERT_PyTorch_SUT():
         model_file = os.environ.get("ML_MODEL_FILE_WITH_PATH", "build/data/bert_tf_v1_1_large_fp32_384_v2/model.pytorch")
         self.model.load_state_dict(torch.load(model_file), strict=False)
 
+        # 量化模型
+        self.model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+        torch.quantization.prepare(self.model, inplace=True)
+        torch.quantization.convert(self.model, inplace=True)
+
         print("Constructing SUT...")
         self.sut = lg.ConstructSUT(self.issue_queries, self.flush_queries)
         print("Finished constructing SUT.")
@@ -66,40 +42,53 @@ class BERT_PyTorch_SUT():
         self.qsl = get_squad_QSL(args.max_examples)
 
     def issue_queries(self, query_samples):
-        for i in range(len(query_samples)):
-            eval_features = self.qsl.get_features(query_samples[i].index)
-            self.process_sample(eval_features, query_samples[i].id)
+        batch_size = 64  # 设置批量大小为64
+        for i in range(0, len(query_samples), batch_size):
+            batch_samples = query_samples[i:i + batch_size]
+            eval_features = [self.qsl.get_features(sample.index) for sample in batch_samples]
+            self.process_sample(eval_features, [sample.id for sample in batch_samples])
 
-    def process_sample(self, sample_input, query_id = None):
+    def process_sample(self, sample_inputs, query_ids=None):
+        input_ids = []
+        input_masks = []
+        segment_ids = []
 
-        if self.network == "sut":
-            input_ids = sample_input['input_ids']
-            input_mask = sample_input['input_mask']
-            segment_ids = sample_input['segment_ids']
-        else:
-            input_ids = sample_input.input_ids
-            input_mask = sample_input.input_mask
-            segment_ids = sample_input.segment_ids
+        for sample_input in sample_inputs:
+            if self.network == "sut":
+                input_ids.append(sample_input['input_ids'])
+                input_masks.append(sample_input['input_mask'])
+                segment_ids.append(sample_input['segment_ids'])
+            else:
+                input_ids.append(sample_input.input_ids)
+                input_masks.append(sample_input.input_mask)
+                segment_ids.append(sample_input.segment_ids)
+
+        input_ids = torch.LongTensor(input_ids).to(self.dev)
+        input_masks = torch.LongTensor(input_masks).to(self.dev)
+        segment_ids = torch.LongTensor(segment_ids).to(self.dev)
 
         with torch.no_grad():
-            model_output = self.model.forward(input_ids=torch.LongTensor(input_ids).unsqueeze(0).to(self.dev),
-                attention_mask=torch.LongTensor(input_mask).unsqueeze(0).to(self.dev),
-                token_type_ids=torch.LongTensor(segment_ids).unsqueeze(0).to(self.dev))
+            model_output = self.model.forward(
+                input_ids=input_ids,
+                attention_mask=input_masks,
+                token_type_ids=segment_ids
+            )
             if self.version >= '4.0.0':
                 start_scores = model_output.start_logits
                 end_scores = model_output.end_logits
             else:
                 start_scores, end_scores = model_output
-            output = torch.stack([start_scores, end_scores], axis=-1).squeeze(0).cpu().numpy()
+
+            outputs = torch.stack([start_scores, end_scores], axis=-1).cpu().numpy()
 
             if self.network == "sut":
-                return output.tolist()
-    
-            response_array = array.array("B", output.tobytes())
-            bi = response_array.buffer_info()
-            response = lg.QuerySampleResponse(query_id, bi[0], bi[1])
-            lg.QuerySamplesComplete([response])
+                return [output.tolist() for output in outputs]
 
+            for i, output in enumerate(outputs):
+                response_array = array.array("B", output.tobytes())
+                bi = response_array.buffer_info()
+                response = lg.QuerySampleResponse(query_ids[i], bi[0], bi[1])
+                lg.QuerySamplesComplete([response])
 
     def flush_queries(self):
         pass
